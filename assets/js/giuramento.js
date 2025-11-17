e7d1a399296fd8cf519d7feef1f9775ee9684dea
Antonio <124215080+Antoniodp087@users.noreply.github.com>
2025-11-17 18:45:11 +0100

    Initial commit del portfolio
    adding portfolio files

---
+++ assets/js/giuramento.js
@@ -0,0 +1,12 @@
+  window.onload = function () {
+                console.log("Web programmers oath\n-Programming is hard, that is why I will use all the possible means to make it easier\n-I will have my browser developer console open all the time\n-I progress with small steps\n-I will write lots of console.log statements to make sure I understand how the code behaves and to help pinpointing problems\n-If my code does not work, I will not write more code. Instead I start deleting the code until it works or just return to a state when everything was still working")
+        };
+
+/*
+Il giuramento dei programmatori web
+Programmare è difficile, ecco perché userò tutti i mezzi possibili per renderlo più semplice
+Avrò la console per sviluppatori del browser sempre aperta
+Procedo a piccoli passi
+Scriverò molte istruzioni console.log per assicurarmi di capire come si comporta il codice e per aiutare a individuare i problemi
+Se il mio codice non funziona, non scriverò altro codice. Invece inizio a eliminare il codice finché non funziona o semplicemente torno a uno stato in cui tutto funzionava ancora
+*/
